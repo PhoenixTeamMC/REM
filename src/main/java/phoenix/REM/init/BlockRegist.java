44d6951cd7634fd25e4c7d911e848562de8df7e4
Elec332 <arnout1998@gmail.com>
2015-02-05 16:34:58 +0100

    Initial commit

---
+++ src/main/java/phoenix/REM/init/BlockRegist.java
@@ -0,0 +1,13 @@
+package phoenix.REM.init;
+
+/**
+ * Created by Elec332 on 5-2-2015.
+ */
+public class BlockRegist {
+
+    public static final BlockRegist instance = new BlockRegist();
+
+    public void init(){
+
+    }
+}
