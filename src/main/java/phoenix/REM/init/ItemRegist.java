a013cd8bcfc65e2f34ddab895c85654f9fd891c3
Elec332 <arnout1998@gmail.com>
2015-02-05 15:48:37 +0100

    Initial commit

---
+++ src/main/java/phoenix/REM/init/ItemRegist.java
@@ -0,0 +1,13 @@
+package phoenix.REM.init;
+
+/**
+ * Created by Elec332 on 5-2-2015.
+ */
+public class ItemRegist {
+
+    public static final ItemRegist instance = new ItemRegist();
+
+    public void init(){
+
+    }
+}
