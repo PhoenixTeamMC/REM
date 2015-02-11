d2643954b37125fc85e733f825b3e60fdcb5ff7d
Elec332 <arnout1998@gmail.com>
2015-02-11 17:49:24 +0100

    Little bit of work on corner, doesnt work yet

--- src/main/java/phoenix/rem/blocks/tile/transmitter/TEWoodCorner.java
+++ src/main/java/phoenix/rem/blocks/tile/transmitter/TEWoodCorner.java
@@ -1,11 +1,28 @@
 package phoenix.rem.blocks.tile.transmitter;
 
+import net.minecraftforge.common.util.ForgeDirection;
 import phoenix.rem.api.power.BaseTileTransmitter;
 
 /**
  * Created by Elec332 on 11-2-2015.
  */
-public class TEWoodCorner extends BaseTileTransmitter {
+public class TEWoodCorner extends BaseTileTransmitter{
+
+    @Override
+    public Boolean canReceivePowerFromSide(ForgeDirection direction) {
+        switch (direction){
+            case WEST:
+                return (output == ForgeDirection.SOUTH || output == ForgeDirection.NORTH);
+            case NORTH:
+                return (output == ForgeDirection.WEST || output == ForgeDirection.EAST);
+            case EAST:
+                return (output == ForgeDirection.NORTH || output == ForgeDirection.SOUTH);
+            case SOUTH:
+                return (output == ForgeDirection.EAST || output == ForgeDirection.WEST);
+            default:
+                return false;
+        }
+    }
 
     @Override
     public Integer maxSpeed() {
